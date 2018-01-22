42b44665643b6aeb25ca48eaa729e7a6c4470c61
James Richardson <richardson.james@outlook.com>
2018-01-22 07:39:33 -0500

    updated ComicBook View data

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,26 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+
+
+
+
         // GET: ComicBooks
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Hogans Hero's";
+            ViewBag.IssueNumber = 1;
+            ViewBag.Description = "<p>The first issue ever</p>";
+            ViewBag.Artists = new string[]
+            {
+                 "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
             return View();
         }
     }
