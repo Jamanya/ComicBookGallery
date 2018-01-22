dcc33f4d468da7ee61133d06a67b0011a30b111d
James Richardson <richardson.james@outlook.com>
2018-01-22 09:05:37 -0500

    added strongly typed models

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
-namespace ComicBookGallery.Controllers
-{
-    public class ComicBooksController : Controller
-    {
+namespace ComicBookGallery.Controllers {
 
 
-
+    public class ComicBooksController : Controller {
 
         // GET: ComicBooks
-        public ActionResult Detail()
-        {
-            ViewBag.SeriesTitle = "Hogans Hero's";
-            ViewBag.IssueNumber = 1;
-            ViewBag.Description = "<p>The first issue ever</p>";
-            ViewBag.Artists = new string[]
-            {
-                 "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+        public ActionResult Detail() {
+            var comicBook = new ComicBook() {
+
+                SeriesTitle = "Hogans Hero's",
+                IssueNumber = 1,
+                DescriptionHtml = "<p>The first issue ever </p>",
+                Artists = new Artists[]
+                {
+                    new Artists() { Name= "Dan Slott", Role= "Script"},
+                    new Artists() { Name= "Humberto Ramos", Role= "Pencils"},
+                    new Artists() { Name= "Victor Olazaba", Role= "Inks"},
+                    new Artists() { Name= "Edgar Delgado", Role= "Colors"},
+                    new Artists() { Name= "Chris Eliopoulos", Role= "Letters"},
+                }
+
             };
-
-
-            return View();
+            return View(comicBook);
         }
     }
 }
